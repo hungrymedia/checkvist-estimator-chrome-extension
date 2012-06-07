8c8dc0857d0fc2bd5add2f933fef9db18d912f2e
Warren Harrison <warren@hungry-media.com>
2012-06-07 14:37:09 -0400

    Implement proper working extension against beta.checkvist.com. Add compiled extension for easier testing.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Checkvist Estimator Chrome Extension
 
-This work in progress chrome extension will ultimately enable the use of [Checkvist.com](http://checkvist.com) to create project estimates.
+This chrome extension enables the use of [Checkvist.com](http://checkvist.com) to create project estimates.
 
-Currently, I use tags to record how long a particular item in a checkvist outline will take (e.g. #hrs-8). The goal of this extension is to tag parent items with the total hours of its children. This is something I'm currently doing manually.
+Currently, I use tags to record how many hours a particular item in a checkvist outline will take (e.g. #hrs-8). This extension will tag parent items with the total hours of its children. 
+
+The extension currently only uses tags with the #hrs- prefix.
+
+## TO DO: 
+- allow for customizable tag prefix or suffix for hours
+- allow for option to use days with customizable tag prefix or suffix
+- allow for choice of created tags in hours, days or both 
+
+
+
--- content_script.js
+++ content_script.js
@@ -1,55 +1,98 @@
-var rootAPIUrl = 'http://beta.checkvist.com/'; 
+var rootAPIUrl = 'https://beta.checkvist.com/'; 
 var url = document.location.toString();
 var urlParts = url.split('/');
 var listID = urlParts.pop().replace('#','');
 console.log("listID: " + listID);
-var taskHrs = {};
-var taskContent = {};
+var allTasks = {};
+var tagPattern = 'hrs-';
 /******************************************************************************
 
 TO DO: 
-Need better method to update ALL levels of list, not just immediate parents of
-those with #hrs tags
+- allow for customizable tag prefix or suffix for hours
+- allow for option to use days with customizable tag prefix or suffix
+- allow for choice of created tags in hours, days or both 
 
 ******************************************************************************/
 
 $.getJSON(rootAPIUrl + 'checklists/' + listID + '/tasks.json').complete( function(data){
 	var listData = JSON.parse(data.responseText);
-	console.log(listData);
 	for(var task in listData){
-		taskContent[listData[task].id] = listData[task].content;
-		if( Object.keys(listData[task].tags).length > 0 ){
-			var taskTags = Object.keys(listData[task].tags);
-			for( var tag in taskTags ){
-				if( taskTags[tag].substr(0, 4) == 'hrs-' ){
-//					console.log(listData[task]);
-					var hours = taskTags[tag].substr(4);
-					if( taskHrs[listData[task].parent_id] == undefined ){
-						taskHrs[listData[task].parent_id] = parseFloat(hours);
-					}else{
-						taskHrs[listData[task].parent_id] += parseFloat(hours);
-					}
-				}
+		var thisID = listData[task].id;
+		allTasks[thisID] = listData[task];
+		allTasks[thisID]['hours'] = 0;
+	}
+	for( var t in allTasks ){
+		if( allTasks[t].parent_id == 0 ){
+			console.log(allTasks[t].id + ' has no parent.');
+			getChildTotal(allTasks[t].id);
+		}
+	}
+	for( var t in allTasks ){
+			updateTaskOnServer(allTasks[t].id);
+	}
+});
+
+function getChildTotal(taskID){
+	var thisTask = allTasks[taskID];
+	console.log('Processing task: ' + thisTask.id);
+	if( thisTask.tasks.length == 0 ){
+		console.log("Task has no children");
+		// has no children, set its hours, clear hours tag
+		var tags = Object.keys(thisTask.tags);
+		console.log(tags);
+		for( var tag in tags ){
+			console.log('Processing tag: ' + tags[tag]);
+			var tagText = tags[tag].toString();
+			if( tagText.substr(0, 4) == tagPattern ){
+				thisTask.hours = parseFloat(tagText.substr(4));
+				console.log('Setting hours to : ' + thisTask.hours);
+			}
+		}
+	}else{
+		for( var child in thisTask.tasks){
+			console.log('Found child task: ' + thisTask.tasks[child]);
+			getChildTotal(thisTask.tasks[child]);
+			thisTask.hours += allTasks[thisTask.tasks[child]].hours;
+		}
+	}
+	if( deleteTagsMatching(thisTask.id, tagPattern) ){
+		thisTask.tags[tagPattern + thisTask.hours] = false;
+	}
+}
+
+function deleteTagsMatching(id, pattern){
+	taskTags = Object.keys( allTasks[id].tags );
+	var numDeletions = 0;
+	for( var t in taskTags ){
+		if( taskTags[t].substr(0, pattern.length) == pattern ){
+			// don't bother to delete if the tag already matches the hours
+			console.log(pattern + ' -- ' + allTasks[id].hours != taskTags[t]);
+			if( pattern + allTasks[id].hours != taskTags[t] ){
+				console.log("deleting tag: " + taskTags[t] + ' from :' + id);
+				delete allTasks[id].tags[taskTags[t]];
+				numDeletions++;
 			}
 		}
 	}
-	console.log(taskHrs);
-	// update parent tasks with calculated hours
-	var taskKeys = Object.keys(taskHrs);
-	for( var key in taskKeys ){
-		updateURL = rootAPIUrl + 'checklists/' + listID + '/tasks/' + taskKeys[key] + '.json';
-		console.log(updateURL);
-		// need to replace any existing #hrs- tags, not simply set the tags, overwriting all
-		$.ajax({
-			type: 'PUT', 
-			url: updateURL,
-			data: {
-				'task': { 
-					'tags':  '#hrs-' + taskHrs[taskKeys[key]] 
-				}
-			}
-		}).complete( function(d){ 
-			console.log(d);
-		});
+	if( numDeletions > 0 ){
+		return true;
+	}else{
+		return false;
 	}
-});
+}
+
+function updateTaskOnServer( taskID ){
+	updateURL = rootAPIUrl + 'checklists/' + listID + '/tasks/' + taskID + '.json';
+	var tagsCommaDelimited = Object.keys( allTasks[taskID].tags ).join(',');
+	$.ajax({
+		type: 'PUT', 
+		url: updateURL,
+		data: {
+			'task': { 
+				'tags':  tagsCommaDelimited
+			}
+		}
+	}).complete( function(d){ 
+		console.log(d);
+	});
+}
